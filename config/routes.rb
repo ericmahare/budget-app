09002104d3ec75d8f0e20032fd9e98bd1b62e4dc
ericmahare <erikmahare@gmail.com>
2022-09-08 22:43:29 +0300

    Include required routes for the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :users do 
+    resources :categories do
+      resources :expenditures
+    end
+  end
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
 end
